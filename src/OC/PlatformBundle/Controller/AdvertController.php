8e8c9f404184412ce92c689912626c125bc250bc
cindy <c.cindycastel@gmail.com>
2018-03-09 16:40:26 +0100

    Ajout d'un controller

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+
+namespace OC\PlatformBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AdvertController
+
+{
+
+public function indexAction(){
+
+	return new Response("Notre propre hello wordl ! ") ; 
+}
+
+}
+
+?>
