ce2cd6a6254ef378eb78dbb5d6bb3af43911fbb7
cindy <c.cindycastel@gmail.com>
2018-03-10 18:50:18 +0100

    Modif controller

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -15,16 +15,58 @@ class AdvertController extends Controller
 
 {
 
-	public function indexAction(){
+	public function indexAction($page){
 
-	$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom'=>'Cindy')) ; 
-		return new Response($content) ; 
+	//il faut au moins une page 
+		if($page <1){
+			// on déclenche une erreur 404
+			throw new NotFoundHttpException('Page "'. $page .'"inexistante.');
+			
+		}
+
+	
+		return $this->render('OCPlatformBundle:Advert:index.html.twig') ; 
 	}
 
 	//consulation d'une annonce 
 	public function viewAction($id){
 
-		return new Response("Affichage de l'annonce d'id:" .$id) ; 
+		return $this->render('OCPlatformBundle:Advert:view.html.twig', array('id' => $id )) ; 
+	}
+
+	//ajout d'une annonce 
+	public function addAction(Request $request){
+
+		if($request->isMethos('POST')){
+
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistré') ; 
+
+			return $this->redirectToRoute('oc_platform_view', array('id' => 5 )) ; 
+
+		}
+
+		return $this->render('OCPlatformBundle:Advert:add.html.twig') ; 
+
+		
+	}
+
+	//edit d'une annonce 
+	public function editAction($id, Request $request){
+
+		if($request->isMethos('POST')){
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifié') ; 
+			return $this->redirectToRoute('oc_platform_view', array('id' => 5 )) ; 
+
+		}
+
+		return $this->render('OCPlatformBundle:Advert:edit.html.twig') ; 
+	}
+
+	// supression d'une annonce 
+
+	public function deleteAction($id){
+
+		return $this->render('OCPlatformBundle:delete:edit.html.twig') ; 
 	}
 
 }
